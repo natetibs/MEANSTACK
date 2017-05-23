d95514a04e4323766e35a96bc4131c210f36498d
natetibs <tibbetts18@up.edu>
2017-05-23 16:57:07 -0600

    helloworld initial

---
+++ server.js
@@ -0,0 +1,47 @@
+console.log('May Node be with you')
+
+const express = require('express')
+const bodyParser= require('body-parser')
+const MongoClient = require('mongodb').MongoClient
+const app = express()
+
+var db
+
+MongoClient.connect('mongodb://localhost:27017/greenBottle', (err, database) => {
+  if (err) return console.log(err)
+  db = database
+  app.listen(3000, () => {
+    console.log('listening on 3000')
+  })
+})
+
+app.use(bodyParser.urlencoded({extended: true}))
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html')
+  // Note: __dirname is directory that contains the JavaScript source code. Try logging it and see what you get!
+  // Mine was '/Users/zellwk/Projects/demo-repos/crud-express-mongo' for this app.
+})
+app.get('/core.js', (req, res) => {
+  console.log("requested core.js")
+  res.sendFile(__dirname + '/core.js')
+})
+app.get('/api/quotes',function(req,res){
+  db.collection('quotes').find().toArray((err, result) => {
+    if (err) return console.log(err)
+    else return res.json(result)
+  });
+  
+
+
+});
+
+app.post('/quotes', function(req,res){
+  db.collection('quotes').save(req.body, (err, result) => {
+    if (err) return console.log(err)
+
+    console.log('saved to database')
+    res.redirect('/')
+  })
+})
+//told you haha
