8a1c830d1bf6d873e8a5730355a7498ec7628100
Preston Kelley <prke9211@colorado.edu>
2017-05-24 10:36:19 -0600

    Fixed bug referencing core.js
    In both server.js and index.html

--- server.js
+++ server.js
@@ -19,12 +19,13 @@ app.use(bodyParser.urlencoded({extended: true}))
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/UI/index.html')
+  console.log("req index")
   // Note: __dirname is directory that contains the JavaScript source code. Try logging it and see what you get!
   // Mine was '/Users/zellwk/Projects/demo-repos/crud-express-mongo' for this app.
 })
 app.get('/UI/core.js', (req, res) => {
   console.log("requested core.js")
-  res.sendFile(__dirname + '/core.js')
+  res.sendFile(__dirname + '/UI/core.js')
 })
 app.get('/api/quotes',function(req,res){
   db.collection('quotes').find().toArray((err, result) => {
